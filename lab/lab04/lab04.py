9e73e33d7edd6c6d8561f2899da3cec935010556
ROOOO <ql.wang.gd@gmail.com>
2022-11-03 22:37:47 +0800

    [Answer] Lab 04: Recursion, Tree Recursion, Python Lists

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -19,6 +19,9 @@ def skip_add(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n <= 1:
+        return n
+    return n + skip_add(n - 2)
 
 
 def summation(n, term):
@@ -41,6 +44,9 @@ def summation(n, term):
     """
     assert n >= 1
     "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(n)
+    return summation(n - 1, term) + term(n)
 
 
 def paths(m, n):
@@ -57,6 +63,11 @@ def paths(m, n):
     1
     """
     "*** YOUR CODE HERE ***"
+    if m == 0 or n == 0:
+        return 0
+    if m == 1 and n == 1:
+        return 1
+    return paths(m - 1, n) + paths(m, n - 1)
 
 
 
@@ -105,6 +116,9 @@ def max_subseq(n, t):
     5
     """
     "*** YOUR CODE HERE ***"
+    if n == 0 or t == 0:
+        return 0
+    return max(max_subseq(n // 10, t - 1) * 10 + n % 10, max_subseq(n // 10, t))
 
 
 def add_chars(w1, w2):
@@ -134,4 +148,11 @@ def add_chars(w1, w2):
     True
     """
     "*** YOUR CODE HERE ***"
+    if w1 == '':
+        return w2
+    if w2 == '':
+        return ''
+    if w1[0] == w2[0]:
+        return add_chars(w1[1:], w2[1:])
+    return w2[0] + add_chars(w1, w2[1:])
 
