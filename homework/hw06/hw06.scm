5c534c33e6a256de6a456742b1c6c945af5c74ac
ROOOO <ql.wang.gd@gmail.com>
2023-04-29 09:27:05 +0800

    [Answer] Homework 6: Scheme

--- homework/hw06/hw06.scm
+++ homework/hw06/hw06.scm
@@ -3,15 +3,21 @@
 
 (define (cadr s)
   'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
   'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
 
 
 (define (sign num)
   'YOUR-CODE-HERE
+  (cond
+    ((< num 0) -1)
+    ((= num 0) 0)
+    (else 1))
 )
 
 
@@ -19,5 +25,11 @@
 
 (define (pow x y)
   'YOUR-CODE-HERE
+  (cond
+    ((= x 1) 1)
+    ((= y 0) 1)
+    ((= y 1) x)
+    ((odd? y) (* x (pow x (- y 1))))
+    (else (square (pow x (/ y 2)))))
 )
 
