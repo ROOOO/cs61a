4f3595c4a6543c50ca607cf87d54342630104356
ROOOO <ql.wang.gd@gmail.com>
2023-05-21 10:36:52 +0800

    [Answer] Lab 12: SQL

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -2,20 +2,20 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color = "blue" AND pet = "dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song FROM students WHERE color = "blue" AND pet = "dog";
 
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest LIMIT 20;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet AS pet, a.song AS song, a.color, b.color FROM students as a, students as b WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven FROM students AS a, numbers AS b WHERE a.time = b.time AND a.number = 7 AND b.'7' = 'True';
 
