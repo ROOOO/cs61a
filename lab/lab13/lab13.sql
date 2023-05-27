500055f1a6399fbd2f9ecdb3a5e138d73a702883
ROOOO <ql.wang.gd@gmail.com>
2023-05-28 01:50:16 +0800

    [Answer] Lab 13: SQL (Optional)

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,17 +2,20 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, AVG(MSRP) AS average_price FROM products GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, MIN(price) FROM inventory GROUP BY item;
 
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT name, store FROM products AS p, lowest_prices AS l
+    WHERE p.name = l.item
+    GROUP BY category HAVING MIN(MSRP / rating);
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT SUM(Mbs) FROM stores AS s, shopping_list AS l
+    WHERE s.store = l.store;
 
